18b1cd56693b962b0d39854a60f6e4b4c41d6e38
Andrew X. Shah <drewshah0@gmail.com>
2023-05-14 20:27:46 -0600

    build(deps): add dialyxir

--- mix.exs
+++ mix.exs
@@ -21,7 +21,8 @@ defmodule Playground.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.3", only: [:dev], runtime: false}
     ]
   end
 end
